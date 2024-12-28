64ebe31b8a70e5f695ea00676fc3460571bb72b3
fatemeh salarzaei <salarzaei96@gmail.com>
2024-12-28 22:23:43 +0330

    Added search functionality to admin and user views for Post, Comment, Bookmark, and Like models

--- blogify/blog/views.py
+++ blogify/blog/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework import filters
 from .models import Category, Tag, Post, Comment, Bookmark, Like
 from .serializers import CategorySerializer, TagSerializer, PostSerializer, CommentSerializer, BookmarkSerializer, LikeSerializer
 
@@ -9,6 +10,8 @@ class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = [IsAdminUser]
+    search_fields = ['title', 'content', 'category__name', 'tags__name']  
+    filter_backends = (filters.SearchFilter,)
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user.author)
@@ -17,6 +20,8 @@ class CommentAdminViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     permission_classes = [IsAdminUser]
+    search_fields = ['content', 'post__title']  
+    filter_backends = (filters.SearchFilter,)
 
 class BookmarkAdminViewSet(viewsets.ModelViewSet):
     queryset = Bookmark.objects.all()
@@ -29,14 +34,19 @@ class LikeAdminViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAdminUser]
 
 # User Views
+
 class PostUserViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    search_fields = ['title', 'content', 'category__name', 'tags__name']  
+    filter_backends = (filters.SearchFilter,)
 
 class CommentUserViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.filter(is_approved=True)
     serializer_class = CommentSerializer
     permission_classes = [IsAuthenticated]
+    search_fields = ['content', 'post__title']  
+    filter_backends = (filters.SearchFilter,)
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
