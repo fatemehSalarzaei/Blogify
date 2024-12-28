4e376ed4b73d19f6a08ac4c35f1972c1369e3abb
fatemeh salarzaei <salarzaei96@gmail.com>
2024-12-28 22:35:13 +0330

    Added automatic API documentation using drf-yasg

--- blogify/blogify/urls.py
+++ blogify/blogify/urls.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Blogify API",
+        default_version='v1',
+        description="API documentation for Blogify project",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@blogify.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+)
 
 urlpatterns = [
+    path('swagger/', schema_view.as_view(), name='swagger'),
     path('admin/', include('admin_portal.urls')), 
     path('user/', include('user_portal.urls')), 
     path('accounts/', include('account.urls')), 
