2de89721767170d15d568d32c8779bb807d3a497
fatemeh salarzaei <salarzaei96@gmail.com>
2024-12-28 22:29:22 +0330

    Added filtering functionality to Post, Comment, Bookmark, and Like views using django-filter

--- blogify/blog/views.py
+++ blogify/blog/views.py
@@ -1,8 +1,18 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework import filters
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Category, Tag, Post, Comment, Bookmark, Like
 from .serializers import CategorySerializer, TagSerializer, PostSerializer, CommentSerializer, BookmarkSerializer, LikeSerializer
+import django_filters
+
+class PostFilter(django_filters.FilterSet):
+    category = django_filters.CharFilter(field_name='category__name', lookup_expr='icontains')
+    created_at = django_filters.DateFromToRangeFilter(field_name='created_at')
+
+    class Meta:
+        model = Post
+        fields = ['category', 'created_at']
 
 # Admin Views
 
@@ -10,8 +20,9 @@ class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = [IsAdminUser]
-    search_fields = ['title', 'content', 'category__name', 'tags__name']  
-    filter_backends = (filters.SearchFilter,)
+    search_fields = ['title', 'content', 'category__name', 'tags__name']
+    filter_backends = (filters.SearchFilter, DjangoFilterBackend)
+    filterset_class = PostFilter
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user.author)
@@ -20,8 +31,9 @@ class CommentAdminViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     permission_classes = [IsAdminUser]
-    search_fields = ['content', 'post__title']  
-    filter_backends = (filters.SearchFilter,)
+    search_fields = ['content', 'post__title']
+    filter_backends = (filters.SearchFilter, DjangoFilterBackend)
+    filterset_fields = ['post']
 
 class BookmarkAdminViewSet(viewsets.ModelViewSet):
     queryset = Bookmark.objects.all()
@@ -38,15 +50,16 @@ class LikeAdminViewSet(viewsets.ModelViewSet):
 class PostUserViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    search_fields = ['title', 'content', 'category__name', 'tags__name']  
-    filter_backends = (filters.SearchFilter,)
+    search_fields = ['title', 'content', 'category__name', 'tags__name']
+    filter_backends = (filters.SearchFilter, DjangoFilterBackend)
+    filterset_class = PostFilter
 
 class CommentUserViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.filter(is_approved=True)
     serializer_class = CommentSerializer
     permission_classes = [IsAuthenticated]
-    search_fields = ['content', 'post__title']  
-    filter_backends = (filters.SearchFilter,)
+    search_fields = ['content', 'post__title']
+    filter_backends = (filters.SearchFilter, DjangoFilterBackend)
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
